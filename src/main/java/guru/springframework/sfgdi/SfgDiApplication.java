6b22c20008055a8f07822ccbbfa856dd668acf56
Chris James <chrisdjames@btinternet.com>
2021-08-01 12:24:56 +0100

    Completed lecture 73. Adds Bean configuration example.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -37,5 +37,4 @@ public class SfgDiApplication {
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 	}
-
 }
